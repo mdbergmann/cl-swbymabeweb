807810df32352cf95c4caaadc5fabaccf5904971
Manfred Bergmann <mb@software-by-mabe.com>
2023-02-05 14:51:41 +0100

    cleanup

--- src/views/projects.lisp
+++ src/views/projects.lisp
@@ -7,15 +7,6 @@
 
 (defparameter *page-title* "Manfred Bergmann | Software Development | Projects")
 
-(defmacro content (content-fun-call)
-  `(htm
-    (:div :id "content"
-          (with-content-table
-            (content-headline #!"projects_headline")
-            (content-subline #!"projects_subline")
-            (with-content-line
-              (str ,content-fun-call))))))
-
 (defun render (content-fun)
   (log:debug "Rendering projects view.")
   (with-page (*page-title*
